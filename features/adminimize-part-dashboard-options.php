9450b663ed2c52bd5e756d104812ffa743b3af86
Eric Teubert <ericteubert@googlemail.com>
2012-07-30 12:47:13 +0200

    refactor: move settings table code into helper method

--- features/adminimize-part-dashboard-options.php
+++ features/adminimize-part-dashboard-options.php
@@ -4,8 +4,6 @@
  * Table wrapper for settings metabox content.
  */
 function adminimize_meta_box_dashboard_options_page() {
-	$user_roles_names = adminimize_get_all_user_roles_names();
-	$user_roles       = adminimize_get_all_user_roles();
 
 	$settings = array(
 		'right_now' => array(
@@ -38,74 +36,11 @@ function adminimize_meta_box_dashboard_options_page() {
 		),
 	);
 
-	?>
-	<table summary="config" class="widefat">
-		<thead>
-			<th>
-				<?php echo __( 'Option', 'adminimize' ); ?>
-			</th>
-			<?php foreach ( $user_roles_names as $role_name ): ?>
-				<th>
-					<?php echo $role_name; ?>
-				</th>
-			<?php endforeach ?>
-		</thead>
-		<tbody>
-			<?php foreach ( $settings as $index => $setting ): ?>
-				<?php $values = adminimize_get_option( $index, array(), 'adminimize_dashboard' ); ?>
-				<tr>
-					<td>
-						<?php echo $setting['title']; ?>
-						<span class="description">(<?php echo $setting['description']; ?>)</span>
-					</td>
-					<?php foreach ( $user_roles as $role ): ?>
-						<td>
-							<input type="checkbox" name="adminimize_dashboard[<?php echo $index ?>][<?php echo $role ?>]" <?php checked( $values[ $role ], 'on' ) ?> >
-						</td>
-					<?php endforeach ?>
-				</tr>
-			<?php endforeach ?>
-		</tbody>
-	</table>
-
-	<h4><?php _e( 'Your own options', 'adminimize' ); ?></h4>
-
-	<p class="description">
-		<?php _e( 'It is possible to add your own IDs or classes from elements and tags. You can find IDs and classes with the FireBug Add-on for Firefox. Assign a value and the associate name per line.', 'adminimize' ); ?>
-	</p>
-
-	<table summary="config_edit_post" class="widefat">
-		<thead>
-			<tr>
-				<th><?php _e( 'ID or class', 'adminimize' ); ?></th>
-				<th><?php _e( 'Option', 'adminimize' ); ?></th>
-			</tr>
-		</thead>
-
-		<tbody>
-			<tr valign="top">
-				<td>
-					<textarea class="code" name="adminimize_dashboard_custom[options]" cols="60" rows="3" style="width: 95%;" ><?php echo adminimize_get_option( 'options', '', 'adminimize_dashboard_custom' ); ?></textarea>
-					<br />
-					<span class="description">
-						<?php _e( 'Possible nomination for ID or class. Separate multiple nominations through a carriage return.', 'adminimize' ); ?>
-					</span>
-				</td>
-				<td>
-					<textarea class="code" name="adminimize_dashboard_custom[values]" cols="60" rows="3" id="_mw_adminimize_own_values" style="width: 95%;" ><?php echo adminimize_get_option( 'values', '', 'adminimize_dashboard_custom' ); ?></textarea>
-					<br />
-					<span class="description">
-						<?php _e( 'Possible IDs or classes. Separate multiple values through a carriage return.', 'adminimize' ); ?>
-					</span>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-
-	<br style="clear: both">
-	<?php submit_button( __( 'Save Changes' ), 'button-primary', 'submit', TRUE ); ?>
-	<br style="clear: both">
-	<?php	
+	$args = array(
+		'option_namespace' => 'adminimize_dashboard',
+		'settings'         => $settings
+	);
+	adminimize_generate_checkbox_table( $args );
 }
 
 function adminimize_add_meta_box_dashboard_options() {
--- inc/helpers.php
+++ inc/helpers.php
@@ -35,3 +35,84 @@ function adminimize_field_with_select( $args ) {
 	do_action( 'after_adminimize_field_with_select' );
 	do_action( 'after_adminimize_field_with_select-' . $settings_name );
 }
+
+function adminimize_generate_checkbox_table( $args ) {
+
+	$defaults = array(
+		'option_namespace' => '',
+		'settings'         => array()
+	);
+	$args = wp_parse_args( $args, $defaults );
+
+	$user_roles_names = adminimize_get_all_user_roles_names();
+	$user_roles       = adminimize_get_all_user_roles();
+
+	?>
+	<table summary="config" class="widefat">
+		<thead>
+			<th>
+				<?php echo __( 'Option', 'adminimize' ); ?>
+			</th>
+			<?php foreach ( $user_roles_names as $role_name ): ?>
+				<th>
+					<?php echo $role_name; ?>
+				</th>
+			<?php endforeach ?>
+		</thead>
+		<tbody>
+			<?php foreach ( $args['settings'] as $index => $setting ): ?>
+				<?php $values = adminimize_get_option( $index, array(), $args['option_namespace'] ); ?>
+				<tr>
+					<td>
+						<?php echo $setting['title']; ?>
+						<span class="description">(<?php echo $setting['description']; ?>)</span>
+					</td>
+					<?php foreach ( $user_roles as $role ): ?>
+						<td>
+							<input type="checkbox" name="<?php echo $args['option_namespace'] ?>[<?php echo $index ?>][<?php echo $role ?>]" <?php checked( $values[ $role ], 'on' ) ?> >
+						</td>
+					<?php endforeach ?>
+				</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
+
+	<h4><?php _e( 'Your own options', 'adminimize' ); ?></h4>
+
+	<p class="description">
+		<?php _e( 'It is possible to add your own IDs or classes from elements and tags. You can find IDs and classes with the FireBug Add-on for Firefox. Assign a value and the associate name per line.', 'adminimize' ); ?>
+	</p>
+
+	<table summary="config_edit_post" class="widefat">
+		<thead>
+			<tr>
+				<th><?php _e( 'ID or class', 'adminimize' ); ?></th>
+				<th><?php _e( 'Option', 'adminimize' ); ?></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr valign="top">
+				<td>
+					<textarea class="code" name="<?php echo $args['option_namespace'] ?>_custom[options]" cols="60" rows="3" style="width: 95%;" ><?php echo adminimize_get_option( 'options', '', $args['option_namespace'] . '_custom' ); ?></textarea>
+					<br />
+					<span class="description">
+						<?php _e( 'Possible nomination for ID or class. Separate multiple nominations through a carriage return.', 'adminimize' ); ?>
+					</span>
+				</td>
+				<td>
+					<textarea class="code" name="<?php echo $args['option_namespace'] ?>_custom[values]" cols="60" rows="3" id="_mw_adminimize_own_values" style="width: 95%;" ><?php echo adminimize_get_option( 'values', '', $args['option_namespace'] . '_custom' ); ?></textarea>
+					<br />
+					<span class="description">
+						<?php _e( 'Possible IDs or classes. Separate multiple values through a carriage return.', 'adminimize' ); ?>
+					</span>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<br style="clear: both">
+	<?php submit_button( __( 'Save Changes' ), 'button-primary', 'submit', TRUE ); ?>
+	<br style="clear: both">
+	<?php	
+}
