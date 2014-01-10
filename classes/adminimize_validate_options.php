505ca59d7d830e4806b3fd81bbbe9bf85a11e9eb
Ralf Albert <neun12@gmail.com>
2014-01-10 12:33:30 +0100

    refactor validation class

--- classes/adminimize_validate_options.php
+++ classes/adminimize_validate_options.php
@@ -8,11 +8,9 @@ class Adminimize_Validate_Options extends Adminimize_Storage
 		if ( ! isset( $_REQUEST['action'] ) || $_REQUEST['action'] != 'update' )
 			return $input;
 
-		$output  = array();
 		$output  = $this->get_option();
 		$widgets = $this->widget_object;
 
-		$common       = new Adminimize_Common();
 		$user_roles   = $common->get_all_user_roles();
 		$used_options = $widgets->get_used_options();
 
@@ -24,61 +22,12 @@ class Adminimize_Validate_Options extends Adminimize_Storage
 			/*
 			 * sanitize checkboxes
 			 */
-			foreach( $user_roles as $role ) {
-
-				$id   = sprintf( '%s_%s', $option, $role );
-
-				if ( ! key_exists( $id, $input ) )
-					$input[ $id ] = array();
-
-				if ( ! key_exists( $id, $output ) )
-					$output[ $id ] = array();
-
-				$output[$id] = array_merge( $output[ $id ], $input[ $id ] );
-
-				foreach ( $output[ $id ] as $key => $value ) {
-
-					if ( isset( $output[ $id ][ $key ] ) && ! key_exists( $key, $input[ $id ] ) )
-						$output[ $id ][ $key ] = false;
-
-					$output[ $id ][ $key ] = (bool) trim( $output[ $id ][ $key ] );
-
-				}
-
-			}
+			$this->sanitize_checkboxes( $user_roles, $option, $input, $output );
 
 			/*
 			 * sanitize custom options
 			 */
-			$custom_option_options = sprintf( 'custom_%s_options', $option );
-			$custom_option_values  = sprintf( 'custom_%s_values', $option );
-
-			if ( key_exists( $custom_option_options, $input ) && key_exists( $custom_option_values, $input ) ) {
-
-				$opts = explode( "\n", $input[ $custom_option_options ] );
-				$vals = explode( "\n", $input[ $custom_option_values ] );
-
-				// make both arrays the same size
-				$c_opts = count( $opts );
-				$c_vals = count( $vals );
-
-				if ( $c_opts > $c_vals ) {
-					$vals = array_pad( $vals, $c_opts, '' );
-				} elseif( $c_vals > $c_opts ) {
-					$opts = array_pad( $opts, $c_vals, '' );
-				}
-
-				// escaping-escaping-escaping!!!oneeleven
-				foreach ( $opts as &$o )
-					$o = esc_attr( trim( $o ) );
-
-				foreach ( $vals as &$v )
-					$v = esc_attr( trim( $v ) );
-
-				$output[ $option . '_custom' ] = array_combine( $opts, $vals );
-
-			}
-			/* end sanitize custom options */
+			$this->sanitize_custom_options( $option, $input, $output );
 
 		}
 
@@ -132,6 +81,70 @@ class Adminimize_Validate_Options extends Adminimize_Storage
 		}
 
 		return $output;
+
+	}
+
+	protected function sanitize_checkboxes( $user_roles, $option, $input, &$output ) {
+
+		foreach( $user_roles as $role ) {
+
+			$id = sprintf( '%s_%s', $option, $role );
+
+			if ( ! key_exists( $id, $input ) )
+				$input[ $id ] = array();
+
+			if ( ! key_exists( $id, $output ) )
+				$output[ $id ] = array();
+
+			$output[$id] = array_merge( $output[ $id ], $input[ $id ] );
+
+			foreach ( $output[ $id ] as $key => $value ) {
+
+				if ( isset( $output[ $id ][ $key ] ) && ! key_exists( $key, $input[ $id ] ) )
+					$output[ $id ][ $key ] = false;
+
+				$output[ $id ][ $key ] = (bool) trim( $output[ $id ][ $key ] );
+
+			}
+
+		}
+
+	}
+
+	protected function sanitize_custom_options( $option, $input, &$output ) {
+
+		$custom_option_options = sprintf( 'custom_%s_options', $option );
+		$custom_option_values  = sprintf( 'custom_%s_values', $option );
+
+		if ( key_exists( $custom_option_options, $input ) && key_exists( $custom_option_values, $input ) ) {
+
+			$opts = explode( "\n", $input[ $custom_option_options ] );
+			$vals = explode( "\n", $input[ $custom_option_values ] );
+
+			// make both arrays the same size
+			$c_opts = count( $opts );
+			$c_vals = count( $vals );
+
+			if ( $c_opts > $c_vals ) {
+
+				$vals = array_pad( $vals, $c_opts, '' );
+
+			} elseif( $c_vals > $c_opts ) {
+
+				$opts = array_pad( $opts, $c_vals, '' );
+
+			}
+
+			// escaping-escaping-escaping!!!oneeleven
+			foreach ( $opts as &$o )
+				$o = esc_attr( trim( $o ) );
+
+			foreach ( $vals as &$v )
+				$v = esc_attr( trim( $v ) );
+
+			$output[ $option . '_custom' ] = array_combine( $opts, $vals );
+
+		}
 
 	}
 
