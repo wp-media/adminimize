054ba4c6cd0288a05a1e30e556cd10cd9ca8e0ff
bueltge <frank@bueltge.de>
2016-01-28 13:46:07 +0100

    Change version for testing

--- adminimize.php
+++ adminimize.php
@@ -7,7 +7,7 @@
  * Description: Visually compresses the administrative meta-boxes so that more admin page content can be initially seen. The plugin that lets you hide 'unnecessary' items from the WordPress administration menu, for all roles of your install. You can also hide post meta controls on the edit-area to simplify the interface. It is possible to simplify the admin in different for all roles.
  * Author:      Frank Bültge
  * Author URI:  http://bueltge.de/
- * Version:     1.9.1
+ * Version:     1.9.2-alpha
  * License:     GPLv2+
  *
  * @package WordPress
