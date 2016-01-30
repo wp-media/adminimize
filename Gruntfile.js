0174285c6a6d67b71cf2b3584fc788ae63ac2132
bueltge <frank@bueltge.de>
2016-01-30 23:11:05 +0100

    Update de language file, usage grunt for creating pot

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,14 +24,36 @@ module.exports = function(grunt) {
 					ext: '.min.css'
 				}]
 			}
+		},
+		makepot: {
+			target: {
+				options: {
+					cwd: '',                          // Directory of files to internationalize.
+					domainPath: 'languages/',               // Where to save the POT file.
+					exclude: [],                      // List of files or directories to ignore.
+					include: ['.*'],                      // List of files or directories to include.
+					mainFile: 'adminimize.php',                     // Main project file.
+					potComments: '',                  // The copyright at the beginning of the POT file.
+					potFilename: 'adminimize-xx_XX.pot',              // Name of the POT file.
+					potHeaders: {
+						poedit: false,                 // Includes common Poedit headers.
+						'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+					},                                // Headers to add to the generated POT file.
+					processPot: null,                 // A callback function for manipulating the POT file.
+					type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
+					updateTimestamp: true,            // Whether the POT-Creation-Date should be updated without other changes.
+					updatePoFiles: false              // Whether to update PO files in the same directory as the POT file.
+				}
+			}
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-wp-i18n');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify', 'cssmin']);
+	grunt.registerTask('default', ['uglify', 'cssmin', 'makepot']);
 
 };
