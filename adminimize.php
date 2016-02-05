3e7ff1ee5dd1b7f3a71097d7e7ec217962093dc6
Frank Bültge <frank@bueltge.de>
2016-02-05 16:09:13 +0100

    new version for testing

--- adminimize.php
+++ adminimize.php
@@ -4,16 +4,16 @@
  * Plugin URI:  https://wordpress.org/plugins/adminimize/
  * Text Domain: adminimize
  * Domain Path: /languages
- * Description: Visually compresses the administrative meta-boxes so that more admin page content can be initially
- * seen. The plugin that lets you hide 'unnecessary' items from the WordPress administration menu, for all roles of
- * your install. You can also hide post meta controls on the edit-area to simplify the interface. It is possible to
- * simplify the admin in different for all roles. Author:      Frank Bültge Author URI:  http://bueltge.de/ Version:
- *  1.9.3-RC2 License:     GPLv3+
+ * Description: Visually compresses the administrative meta-boxes so that more admin page content can be initially seen. The plugin that lets you hide 'unnecessary' items from the WordPress administration menu, for all roles of your install. You can also hide post meta controls on the edit-area to simplify the interface. It is possible tosimplify the admin in different for all roles.
+ * Author:      Frank Bültge
+ * Author URI:  http://bueltge.de/
+ * Version:     1.10.0-alpha
+ * License:     GPLv3+
  *
  * @package WordPress
  * @author  Frank Bültge <frank@bueltge.de>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version 2016-02-03
+ * @version 2016-02-05
  */
 
 /**
