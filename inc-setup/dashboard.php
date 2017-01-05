c1004eb46cffe9f42ad4d87f44447a601e63c953
bueltge <frank@bueltge.de>
2017-01-05 12:39:45 +0100

    Allow to hide dashboard custom options

--- inc-setup/dashboard.php
+++ inc-setup/dashboard.php
@@ -4,6 +4,7 @@
  * @subpackage  Dashboard Setup
  * @author      Frank Bültge
  */
+
 if ( ! function_exists( 'add_action' ) ) {
 	echo "Hi there!  I'm just a part of plugin, not much I can do when called directly.";
 	exit;
@@ -14,7 +15,7 @@ if ( ! is_admin() ) {
 }
 
 // If is AJAX Call.
-if ( defined('DOING_AJAX') && DOING_AJAX ) {
+if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 	return;
 }
 
@@ -68,22 +69,72 @@ function _mw_adminimize_dashboard_setup() {
 	$widgets = _mw_adminimize_get_dashboard_widgets();
 	// Get current user data.
 	$user = wp_get_current_user();
+	// If the current user is not inside the roles, abort.
+	if ( ! $user->roles ) {
+		return;
+	}
+
 	foreach ( $user_roles as $role ) {
 
 		if ( is_array( $user->roles )
-			&& is_array( $disabled_dashboard_option_[ $role ] )
-			&& in_array( $role, $user->roles )
-			&& _mw_adminimize_current_user_has_role( $role )
+		     && is_array( $disabled_dashboard_option_[ $role ] )
+		     && in_array( $role, $user->roles, TRUE )
+		     && _mw_adminimize_current_user_has_role( $role )
 		) {
-			foreach ( $disabled_dashboard_option_[ $role ] as $widget ) {
+			foreach ( (array) $disabled_dashboard_option_[ $role ] as $widget ) {
 				if ( isset( $widgets[ $widget ][ 'context' ] ) ) {
 					remove_meta_box( $widget, 'dashboard', $widgets[ $widget ][ 'context' ] );
 				}
 			}
 		}
-
 	}
 
+}
+
+add_action( 'admin_head-index.php', '_mw_adminimize_remove_custom_panels', 99 );
+/**
+ * Add custom options to the head head to hide it via css.
+ *
+ * @since 2017-01-05
+ */
+function _mw_adminimize_remove_custom_panels() {
+
+	// exclude super admin
+	if ( _mw_adminimize_exclude_super_admin() ) {
+		return;
+	}
+
+	$options = _mw_adminimize_get_option_value( '_mw_adminimize_own_dashboard_values' );
+
+	if ( empty( $options ) ) {
+		return;
+	}
+
+	// Get current user data.
+	$user = wp_get_current_user();
+	if ( ! $user->roles ) {
+		return;
+	}
+
+	// Get settings for the roles.
+	$disabled_dashboard_option_ = array();
+	foreach ( $user->roles as $role ) {
+		$disabled_dashboard_option_[] = _mw_adminimize_get_option_value( 'mw_adminimize_disabled_dashboard_option_' . $role . '_items' );
+	}
+
+	// Support Multiple Roles for users.
+	if ( _mw_adminimize_get_option_value( 'mw_adminimize_multiple_roles' ) && 1 < count( $user->roles ) ) {
+		$disabled_dashboard_option_ = _mw_adminimize_get_duplicate( $disabled_dashboard_option_ );
+	}
+
+	if ( empty( $disabled_dashboard_option_[ 0 ] ) ) {
+		return;
+	}
+	_mw_adminimize_debug( $disabled_dashboard_option_[ 0 ] );
+
+	$selectors = implode( ', ', $disabled_dashboard_option_[ 0 ] );
+	echo '<!-- Set Adminimize dashboard options -->' . "\n";
+	echo '<style type="text/css">' . esc_attr( $selectors ) . ' {display:none !important;}</style>' . "\n";
 }
 
 /**
@@ -100,16 +151,16 @@ function _mw_adminimize_get_dashboard_widgets() {
 		return $widgets;
 	}
 
-	foreach ( $wp_meta_boxes[ 'dashboard' ] as $context => $datas ) {
-		foreach ( $datas as $priority => $data ) {
-			foreach ( $data as $widget => $value ) {
+	foreach ( (array) $wp_meta_boxes[ 'dashboard' ] as $context => $datas ) {
+		foreach ( (array) $datas as $priority => $data ) {
+			foreach ( (array) $data as $widget => $value ) {
 				$widgets[ $widget ] = array(
 					'id'       => $widget,
 					'title'    => strip_tags(
 						preg_replace( '/( |)<span.*span>/im', '', $value[ 'title' ] )
 					),
 					'context'  => $context,
-					'priority' => $priority
+					'priority' => $priority,
 				);
 			}
 		}
