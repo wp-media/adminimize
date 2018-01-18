da246e538ea333c8146943c8c24015325217544a
bueltge <frank@bueltge.de>
2018-01-18 20:49:23 +0100

    sanitize all outputs lately

--- src/Templates/SettingsPage.php
+++ src/Templates/SettingsPage.php
@@ -1,25 +1,28 @@
-<?php declare( strict_types = 1 ); # -*- coding: utf-8 -*-
+<?php declare( strict_types=1 ); # -*- coding: utf-8 -*-
 
 ?>
 
 <div class="wrap">
-    <h1><?php echo $this->page_title; ?></h1>
+	<h1><?php echo esc_attr( $this->page_title ); ?></h1>
 
-    <div id="nav-tabs">
-        <h2 class="nav-tab-wrapper">
-            <ul>
-            <?php foreach ( $this->tabs as $key => $tab ): ?>
-                <li>
-                    <a class="nav-tab <?php echo $key === 0 ? 'nav-tab-active' : ''; ?>" href="#tab-<?php echo $key; ?>"><?php echo esc_html( $tab->get_tab_title() ); ?> </a>
-                </li>
-            <?php endforeach; ?>
-            </ul>
-        </h2>
+	<div id="nav-tabs">
+		<h2 class="nav-tab-wrapper">
+			<ul>
+				<?php foreach ( (array) $this->tabs as $key => $tab ) { ?>
+					<li>
+						<a class="nav-tab <?php echo $key === 0 ? 'nav-tab-active' : ''; ?>"
+							href="#tab-<?php echo esc_attr( $key ); ?>">
+							<?php echo esc_html( $tab->get_tab_title() ); ?>
+						</a>
+					</li>
+				<?php } ?>
+			</ul>
+		</h2>
 
-        <?php foreach ( $this->tabs as $key => $tab ): ?>
-            <div id="tab-<?php echo $key; ?>">
-                <?php echo $tab->render_tab_content(); ?> 
-            </div>
-        <?php endforeach; ?>
-    </div>
+		<?php foreach ( (array) $this->tabs as $key => $tab ) { ?>
+			<div id="tab-<?php echo esc_attr( $key ); ?>">
+				<?php echo esc_attr( $tab->render_tab_content() ); ?>
+			</div>
+		<?php } ?>
+	</div>
 </div>
