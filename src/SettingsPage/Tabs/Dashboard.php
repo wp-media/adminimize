6e2954d276f895fa2bfb0ba245f96a7f7db16ee7
bueltge <frank@bueltge.de>
2018-01-18 20:33:25 +0100

    use always tabs instead of spaces

--- src/SettingsPage/Tabs/Dashboard.php
+++ src/SettingsPage/Tabs/Dashboard.php
@@ -1,4 +1,4 @@
-<?php declare( strict_types = 1 ); # -*- coding: utf-8 -*-
+<?php declare( strict_types=1 ); # -*- coding: utf-8 -*-
 
 namespace Adminimize\SettingsPage\Tabs;
 
@@ -8,37 +8,41 @@ use Adminimize\SettingsPage;
  * Stub: Tab for Dashboard Settings.
  */
 class Dashboard implements TabInterface {
-    /**
-     * Holds an instance of the settings page
-     *
-     * @var SettingsPageInterface  
-     */
-    private $settings_page;
 
-    /**
-     * Constructor.
-     *
-     * @param SettingsPage\SettingsPageInterface $settings_page
-     */
-    public function __construct( SettingsPage\SettingsPageInterface $settings_page ) {
-        $this->settings_page = $settings_page;
-    }
+	/**
+	 * Holds an instance of the settings page
+	 *
+	 * @var SettingsPageInterface
+	 */
+	private $settings_page;
 
-    /**
-     * Get display title for the tab.
-     *
-     * @return string
-     */
-    public function get_tab_title() : string {
-        return esc_html_x( 'Dashboard', 'Tab Title', 'adminimize' );
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param SettingsPage\SettingsPageInterface $settings_page
+	 */
+	public function __construct( SettingsPage\SettingsPageInterface $settings_page ) {
 
-    /**
-     * Render content of the tab.
-     *
-     * @return void
-     */
-    public function render_tab_content() {
-        include $this->settings_page->get_template_path() . '/Templates/Dashboard.php';
-    }
+		$this->settings_page = $settings_page;
+	}
+
+	/**
+	 * Get display title for the tab.
+	 *
+	 * @return string
+	 */
+	public function get_tab_title(): string {
+
+		return esc_html_x( 'Dashboard', 'Tab Title', 'adminimize' );
+	}
+
+	/**
+	 * Render content of the tab.
+	 *
+	 * @return void
+	 */
+	public function render_tab_content() {
+
+		include $this->settings_page->get_template_path() . '/Templates/Dashboard.php';
+	}
 }
