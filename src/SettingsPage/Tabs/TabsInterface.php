d49600d680bc2b531cb88f66c00c7d7099d3c737
bueltge <frank@bueltge.de>
2018-01-18 20:43:40 +0100

    use always tabs instead of spaces

--- src/SettingsPage/Tabs/TabsInterface.php
+++ src/SettingsPage/Tabs/TabsInterface.php
@@ -1,4 +1,4 @@
-<?php declare( strict_types = 1 ); # -*- coding: utf-8 -*-
+<?php declare( strict_types=1 ); # -*- coding: utf-8 -*-
 
 namespace Adminimize\SettingsPage\Tabs;
 
@@ -6,10 +6,11 @@ namespace Adminimize\SettingsPage\Tabs;
  * Interface for Tabs class.
  */
 interface TabsInterface {
-    /**
-     * Returns array with namespaced class names of all tabs.
-     *
-     * @return array
-     */
-    public function get_tabs_list() : array;
+
+	/**
+	 * Returns array with namespaced class names of all tabs.
+	 *
+	 * @return array
+	 */
+	public function get_tabs_list(): array;
 }
