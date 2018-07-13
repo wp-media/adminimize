013ca734720352a5853431e630940b406e17512d
bueltge <frank@bueltge.de>
2018-07-13 16:44:24 +0200

    Prepare for pull request 105

--- adminimize.php
+++ adminimize.php
@@ -15,7 +15,7 @@
  * @package WordPress
  * @author  Frank Bültge <frank@bueltge.de>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version 2018-01-29
+ * @version 2018-07-13
  */
 
 /**
