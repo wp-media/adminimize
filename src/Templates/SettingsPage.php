1944bef8960c199f8152de4d8d1e85bbebb0da76
dugajean <dugagjin.s@gmail.com>
2018-11-08 22:36:54 +0000

    WIP: Proper tab design.

--- src/Templates/SettingsPage.php
+++ src/Templates/SettingsPage.php
@@ -3,24 +3,18 @@
 <div class="wrap">
     <h1><?= esc_attr($this->pageTitle); ?></h1>
 
-    <div id="nav-tabs">
-        <h2 class="nav-tab-wrapper">
-            <ul>
-                <?php foreach ((array)$this->tabs as $key => $tab) : ?>
-                    <li>
-                        <a class="nav-tab <?php echo $key === 0 ? 'nav-tab-active' : ''; ?>"
-                            href="#tab-<?= esc_attr($key); ?>">
-                            <?= esc_html($tab->title()); ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </h2>
-
+    <h2 class="nav-tab-wrapper">
         <?php foreach ((array)$this->tabs as $key => $tab) : ?>
-            <div id="tab-<?php echo esc_attr($key) ?>">
-                <?= esc_attr($tab->render()) ?>
-            </div>
+            <a class="nav-tab <?php echo $key === 0 ? 'nav-tab-active' : ''; ?>"
+                href="#tab-<?= esc_attr($key); ?>">
+                <?= esc_html($tab->title()); ?>
+            </a>
         <?php endforeach; ?>
-    </div>
+    </h2>
+
+    <?php foreach ((array)$this->tabs as $key => $tab) : ?>
+        <div id="tab-<?= esc_attr($key) ?>">
+            <?= esc_attr($tab->render()) ?>
+        </div>
+    <?php endforeach; ?>
 </div>
