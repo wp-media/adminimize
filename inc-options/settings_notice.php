38798c9a7943e14d84b44ef493930c53e96a1be3
Frank Bültge <frank@bueltge.de>
2022-09-30 10:59:39 +0200

    Update settings_notice.php

--- inc-options/settings_notice.php
+++ inc-options/settings_notice.php
@@ -4,13 +4,18 @@
  * @subpackage Notice for settings page
  * @author     Frank Bültge
  */
+
 if ( ! function_exists( 'add_filter' ) ) {
 	echo "Hi there! I'm just a part of plugin, not much I can do when called directly.";
 	exit;
 }
-// always visible
+
+// always visible.
 add_action( 'load-settings_page_adminimize/adminimize', '_mw_adminimize_add_settings_error' );
 
+/**
+ * Add custom errer messages for the error notes.
+ */
 function _mw_adminimize_add_settings_error() {
 
 	$settings_hint_message = '<span style="font-size: 35px; float: left; margin: 10px 3px 0 0;">&#x261D;</span>'
